37cfa64e401e105cc2469d66b70602b54c15647e
mugabo22 <ronaldmugabo424@gmail.com>
2023-09-09 09:18:11 +0300

    learn_python

---
+++ functions.py
@@ -0,0 +1,23 @@
+def animation():
+    first_name="lion"
+    age=20
+    weslth=22.0
+    are_you_married=True
+    print(first_name)
+    print(weslth)
+    print(are_you_married)
+animation()    
+
+def mylife():
+    name="ronald"
+    age=20
+    course="python"
+    expected_salary=4.7
+    are_you_married=False
+    print(name)
+    print(age)
+    print(course)
+    print(expected_salary)
+    print(are_you_married)
+mylife()    
+
--- second.py
+++ second.py
@@ -1,2 +1,6 @@
-def sec():
-    print("hellow world")
+def mags():
+    y+h=42
+    y-a=5
+    y+a+h=b
+    print(b)
+mags()

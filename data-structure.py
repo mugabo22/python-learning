3d9a683f4923fa9f2bf07978766017e57b2d9424
mugabo22 <ronaldmugabo424@gmail.com>
2023-09-11 17:15:23 +0300

    data-structure.py

---
+++ data-structure.py
@@ -0,0 +1,7 @@
+def work():
+    status=input('are you married? ')
+    age=int(input('how old are you?: '))
+    name=input('whats your name?: ')
+
+    print(f'my name is {name} and {status} am married and am {age}')
+work()    

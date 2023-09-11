a5c663830d272eafadec509f966dfd8fb1f1df07
mugabo22 <ronaldmugabo424@gmail.com>
2023-09-11 18:51:12 +0300

    data-structure.py

--- data-structure.py
+++ data-structure.py
@@ -5,3 +5,12 @@ def work():
 
     print(f'my name is {name} and {status} am married and am {age}')
 work()    
+
+def add(n1,n2,n3):
+    message= n1+n2-n3
+    return message
+
+number_one=int(input("enter your fist number:"))
+number_two=int(input("enter your second number: "))
+number_three=int(input("enter your third number:" ))
+print(add(number_one,number_two,number_three))
